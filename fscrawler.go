18a23e602bd727aa36d6d7c274ad1837460de45c
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2017-01-09 20:57:33 +0200

    Поиск дубликатов

--- fscrawler.go
+++ fscrawler.go
@@ -7,6 +7,9 @@ import (
     "fmt"
     "container/list"
     "bytes"
+    "crypto/md5"
+    "io"
+    "encoding/hex"
 )
 
 
@@ -46,10 +49,69 @@ func ListToString(l *list.List) string {
     return bs.String()
 }
 
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func Investigate(l *list.List) {
+    similar := make(map[string]*list.List)
+    for e := l.Front(); e != nil; e = e.Next() {
+        if path, ok := e.Value.(string); ok {
+            hash, _ := HashFile(path)
+            if _, ok := similar[hash]; !ok {
+                similar[hash] = list.New()
+            }
+            similar[hash].PushBack(path)
+        }
+    }
+
+    for _, files := range similar {
+        if files.Len() > 1 {
+            fmt.Println("Duplicated files: ", ListToString(files))
+        }
+    }
+}
+
+//http://www.mrwaggel.be/post/generate-md5-hash-of-a-file/
+func HashFile(filePath string) (string, error) {
+    //Initialize variable returnMD5String now in case an error has to be returned
+    var returnMD5String string
+
+    //Open the passed argument and check for any error
+    file, err := os.Open(filePath)
+    if err != nil {
+        return returnMD5String, err
+    }
+
+    //Tell the program to call the following function when the current function returns
+    defer file.Close()
+
+    //Open a new hash interface to write to
+    hash := md5.New()
+
+    //Copy the file in the hash interface and check for any error
+    if _, err := io.Copy(hash, file); err != nil {
+        return returnMD5String, err
+    }
+
+    //Get the 16 bytes hash
+    //hashInBytes := hash.Sum(nil)[:16]
+    hashInBytes := hash.Sum(nil)
+
+    //Convert the bytes to a string
+    returnMD5String = hex.EncodeToString(hashInBytes)
+
+    return returnMD5String, nil
+}
+
 func main() {
     var cr = Crawler{Sizes: make(map[int64]*list.List)}
-    filepath.Walk("/home/yevgen/Music", cr.Visitor)
-    for size, items := range cr.Sizes {
-        fmt.Println(size, " (", items.Len(), "): ", ListToString(items))
+    filepath.Walk("/home/yevgen/GoglandProjects/learning-go/fscrawler-sandbox", cr.Visitor)
+
+    for _, items := range cr.Sizes {
+        Investigate(items)
+        //fmt.Println(size, " (", items.Len(), "): ", ListToString(items))
     }
 }
