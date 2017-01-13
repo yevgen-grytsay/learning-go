1117974bd9590bf47b7f667c2ae1b962d5c92438
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2017-01-13 12:52:47 +0200

    Поправил форматирование кода

--- concurrent_gets.go
+++ concurrent_gets.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-    urls := []string{
-        "http://httpbin.orgs",
-        "http://httpbin.org",
-        "http://httpbin.org/ip",
-    }
+	urls := []string{
+		"http://httpbin.orgs",
+		"http://httpbin.org",
+		"http://httpbin.org/ip",
+	}
 
-    ch := make(chan string)
-    for _, url := range urls {
-        go MakeRequest(url, ch)
-    }
-    for range urls {
-        fmt.Println(<-ch)
-    }
+	ch := make(chan string)
+	for _, url := range urls {
+		go MakeRequest(url, ch)
+	}
+	for range urls {
+		fmt.Println(<-ch)
+	}
 }
 
-func MakeRequest(url string, ch chan<-string) {
-    defer func() {
-        if err := recover(); err != nil {
-            ch <- fmt.Sprintf("Could not fetch url")
-        }
-    }()
-    resp, _ := http.Get(url)
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    ch <- fmt.Sprintf("Body from url '%s' is %d bytes long", url, len(body))
-    //if err == nil {
-    //    ch <- fmt.Sprintf("Could not fetch url '%s'", url)
-    //} else {
-    //
-    //}
+func MakeRequest(url string, ch chan<- string) {
+	defer func() {
+		if err := recover(); err != nil {
+			ch <- fmt.Sprintf("Could not fetch url")
+		}
+	}()
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	ch <- fmt.Sprintf("Body from url '%s' is %d bytes long", url, len(body))
+	//if err == nil {
+	//    ch <- fmt.Sprintf("Could not fetch url '%s'", url)
+	//} else {
+	//
+	//}
 }
